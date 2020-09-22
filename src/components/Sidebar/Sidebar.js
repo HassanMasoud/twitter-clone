f7ba3d9530b31ec9d1c86ca69e62bee93deafd20
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-22 23:41:13 +0300

    Add sidebar styles

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,21 +6,22 @@ import SidebarOption from "../SidebarOption/SidebarOption";
 
 function Sidebar() {
   return (
-    <div>
-      <div className="sidebar">
-        <FontAwesomeIcon icon={["fab", "twitter"]} />
-        <SidebarOption active icon="home" text="Home" />
-        <SidebarOption icon="hashtag" text="Explore" />
-        <SidebarOption icon="bell" text="Notifications" />
-        <SidebarOption icon="envelope" text="Messages" />
-        <SidebarOption icon="bookmark" text="Bookmarks" />
-        <SidebarOption icon="list-alt" text="Lists" />
-        <SidebarOption icon="user" text="Profile" />
-        <SidebarOption icon="ellipsis-h" text="More" />
-        <Button variant="outlined" className="sidebar__tweet" fullWidth>
-          Tweet
-        </Button>
-      </div>
+    <div className="sidebar">
+      <FontAwesomeIcon
+        icon={["fab", "twitter"]}
+        className="sidebar__twitterIcon"
+      />
+      <SidebarOption active icon="home" text="Home" />
+      <SidebarOption icon="hashtag" text="Explore" />
+      <SidebarOption icon="bell" text="Notifications" />
+      <SidebarOption icon="envelope" text="Messages" />
+      <SidebarOption icon="bookmark" text="Bookmarks" />
+      <SidebarOption icon="list-alt" text="Lists" />
+      <SidebarOption icon="user" text="Profile" />
+      <SidebarOption icon="ellipsis-h" text="More" />
+      <Button variant="outlined" className="sidebar__tweet" fullWidth>
+        Tweet
+      </Button>
     </div>
   );
 }
