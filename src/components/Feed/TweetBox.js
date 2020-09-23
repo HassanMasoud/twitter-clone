624569fcd3acdf00e4dce621cdf6bf9c195cc171
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-23 23:30:12 +0300

    Post tweets to the database

--- src/components/Feed/TweetBox.js
+++ src/components/Feed/TweetBox.js
@@ -1,23 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import "./TweetBox.css";
 import { Avatar, Button } from "@material-ui/core";
+import db from "../../firebase";
 
 function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+    db.collection("posts").add({
+      displayName: "Hassan Masoud",
+      username: "hassanmasoud",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://pbs.twimg.com/profile_images/448098642914594816/5QxXRobP_reasonably_small.jpeg",
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
   return (
     <div className="tweetBox">
-      <form action="">
+      <form action="" onSubmit={sendTweet}>
         <div className="tweetBox__input">
           <Avatar src="https://pbs.twimg.com/profile_images/448098642914594816/5QxXRobP_reasonably_small.jpeg" />
-          <input type="text" placeholder="What's happening?" />
+          <input
+            type="text"
+            placeholder="What's happening?"
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+          />
         </div>
         <input
+          onChange={(e) => setTweetImage(e.target.value)}
+          value={tweetImage}
           type="text"
-          name=""
-          id=""
           className="tweetBox__imageInput"
           placeholder="Enter image URL"
         />
-        <Button className="tweetBox__tweetButton">Tweet</Button>
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
+          Tweet
+        </Button>
       </form>
     </div>
   );
