f3ad3eb6f50d4a3f50fd35e133d80dfb2fa639b4
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-23 23:05:14 +0300

    Pull posts from firebase

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
+import db from "../../firebase";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) => {
+      setPosts(snapshot.docs.map((doc) => doc.data()));
+    });
+  }, []);
+
   return (
     <div className="feed">
       <div className="feed__header">
         <h2>Home</h2>
       </div>
       <TweetBox />
-      <Post
-        displayName="Los Angeles Lakers"
-        username="Lakers"
-        verified
-        text="We stand with our players and the players of the NBA in their demand for justice and the end of racial violence."
-        image="https://pbs.twimg.com/card_img/1306400608644022273/G6zzgiOc?format=jpg&name=medium"
-        avatar="https://pbs.twimg.com/profile_images/1298765301979336704/GB7KuBvA_reasonably_small.jpg"
-      />
+      {posts.map((post) => (
+        <Post
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          image={post.image}
+          avatar={post.avatar}
+        />
+      ))}
     </div>
   );
 }
